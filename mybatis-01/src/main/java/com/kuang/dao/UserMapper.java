5110c3dd08bc690f1f02556ef04ecfa9ab00bcc8
helloworld <redloveandten@gmail.com>
2024-06-13 23:01:10 +0800

    Changes

--- mybatis-01/src/main/java/com/kuang/dao/UserMapper.java
+++ mybatis-01/src/main/java/com/kuang/dao/UserMapper.java
@@ -3,14 +3,24 @@ package com.kuang.dao;
 import com.kuang.pojo.User;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public interface UserMapper {
     //查詢全部用戶
     List<User> getUserList();
     //用id查
     User getUserById(int id);
+    User getUserById2(int id);
+
     //增加用戶
     int addUser(User user);
+    //萬能map,非正規用法,但很方便
+    int addUser2(Map<String, Object> map);
+
     //修改用戶
     int updateUser(User user);
+
+    int deleteUser(int id);
+
 }
--- mybatis-01/src/test/java/com/kuang/dao/UserDaoTest.java
+++ mybatis-01/src/test/java/com/kuang/dao/UserDaoTest.java
@@ -5,6 +5,7 @@ import com.kuang.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class UserDaoTest {
@@ -49,6 +50,55 @@ public class UserDaoTest {
         //方式一：getMapper
         UserMapper userMapper= sqlSession.getMapper(UserMapper.class);
         userMapper.addUser(new User(6, "hi", "123"));
+
+        //提交事物
+        sqlSession.commit();
+        //關閉SqlSession
+        sqlSession.close();
+    }
+
+    @Test
+    public void addUser2(){
+        //獲取SqlSession對象
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+
+        //方式一：getMapper
+        UserMapper userMapper= sqlSession.getMapper(UserMapper.class);
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("userid",7);
+        map.put("username","he");
+        map.put("pwd","22222");
+
+        System.out.println(userMapper.addUser2(map));
+        //提交事物
+        sqlSession.commit();
+        //關閉SqlSession
+        sqlSession.close();
+    }
+
+    @Test
+    public void updateUser(){
+        //獲取SqlSession對象
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+
+        //方式一：getMapper
+        UserMapper userMapper= sqlSession.getMapper(UserMapper.class);
+        userMapper.updateUser(new User(6, "hoooo", "123"));
+
+        //提交事物
+        sqlSession.commit();
+        //關閉SqlSession
+        sqlSession.close();
+    }
+
+    @Test
+    public void deleteUser(){
+        //獲取SqlSession對象
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+
+        //方式一：getMapper
+        UserMapper userMapper= sqlSession.getMapper(UserMapper.class);
+        userMapper.deleteUser(7);
 
         //提交事物
         sqlSession.commit();
